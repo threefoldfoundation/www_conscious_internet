986dcd4d5445ac5982088b53c0509b8832483944
samaradel <samar@incubaid.com>
2021-02-15 12:47:45 +0200

    Fix pagination

--- src/templates/Team.vue
+++ src/templates/Team.vue
@@ -1,10 +1,5 @@
 <template>
   <Layout>
-    <TagFilterHeader
-      :tags="memberships"
-      selected="all"
-      v-if="$page.memberships.edges.length > 1"
-    />
     <div class="container sm:pxi-0 mx-auto mt-8 overflow-x-hidden">
       <div class="flex flex-wrap with-large pt-8 pb-8 mx-4 sm:-mx-4">
         <PostListItem
@@ -13,14 +8,34 @@
           :record="person.node"
         />
       </div>
+      <div class="text-center" v-if="$page.entries.edges.length == 0">
+        <h2 class="inlibe-flex mx-auto text-gray-700 w-3/4">No results</h2>
+      </div>
+    </div>
+
+    <div class="pagination flex justify-center mb-8">
+      <Pagination
+        v-if="
+          $page.entries.pageInfo.totalPages > 1 &&
+          $page.entries.edges.length > 0
+        "
+        :baseUrl="baseurl"
+        :currentPage="$page.entries.pageInfo.currentPage"
+        :totalPages="$page.entries.pageInfo.totalPages"
+        :maxVisibleButtons="5"
+      />
     </div>
   </Layout>
 </template>
 
 <page-query>
-query ($private: Int){
-  entries: allPerson (sortBy: "rank", order: DESC, filter: { private: { ne: $private }, memberships: { id: {in: ["foundation", "tech", "cofounders"]}}}){
+query($page: Int){
+  entries: allPerson (perPage: 10, page: $page, sortBy: "rank", order: DESC, filter: { memberships: { id: {in: ["foundation", "tech"]}}}) @paginate{
     totalCount
+    pageInfo {
+      totalPages
+      currentPage
+    }
     edges {
       node {
         path
@@ -39,7 +54,7 @@ query ($private: Int){
     }
   }
 
-   memberships: allMembership(filter: {title: {in: ["foundation", "tech", "cofounders"]}}){
+  memberships: allMembership(filter: {title: {in: ["foundation", "tech"]}}){
      edges{
       node{
         id
@@ -53,17 +68,13 @@ query ($private: Int){
 
 <script>
 import PostListItem from "~/components/custom/Cards/PostListItem.vue";
-import TagFilterHeader from "~/components/custom/TagFilterHeader.vue";
+import Pagination from "~/components/custom/Pagination.vue";
 
 export default {
   components: {
     PostListItem,
     TagFilterHeader,
-  },
-  metaInfo() {
-    return {
-      title: this.pageName,
-    };
+    Pagination,
   },
   computed: {
     memberships() {
@@ -73,10 +84,8 @@ export default {
       );
       return res;
     },
-    pageName() {
-      let path = this.$route.path.substring(1);
-      let name = path[0].toUpperCase() + path.slice(1);
-      return name;
+    baseurl() {
+      return "/team/";
     },
   },
 };
