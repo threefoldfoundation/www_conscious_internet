0c44ba620a79005d67e37ee56c07f27dad2ca352
Vilnite <vilnitekristine@gmail.com>
2021-02-23 17:35:25 +0200

    updated tab

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    siteName: 'Alliance for a Conscious Internet',
+    siteName: 'ACI',
     plugins: [
 
         // Local models
